256c3856a47dbd9dcf206f2cfa2525454a5f44b9
Thien phuc <thienphuc484@gmail.com>
2026-03-14 10:34:54 +0700

    Center rect to y and x to left of screen

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,34 @@
-#include <iostream>
-#include <SDL3/SDL.h>
-#include <SDL3/SDL_main.h>
-#include <string>
+
+#include <SDL3/SDL_events.h>
+#include <SDL3/SDL_rect.h>
+#include <SDL3/SDL_render.h>
 
 #include "Game.h"
 
 class Game1 : public Game {
-
     SDL_FRect rect{};
 
-    void start() override {
-        rect = { 200, 150, 400, 400};
-    }
+    const float spawnInterval = 2.0f;
+    float spawnTimer = 0.0f;
+
+    void start() override { rect = {0, 0, (screenWidth - 400.0f) / 2, (screenHeight - 400.0f) / 2}; }
 
     void update() override {
         SDL_PumpEvents();
 
-        const bool *state = SDL_GetKeyboardState(nullptr);
+        const bool* state = SDL_GetKeyboardState(nullptr);
 
         if (state[SDL_SCANCODE_W]) rect.y -= 50.0f * deltaTime;
         if (state[SDL_SCANCODE_S]) rect.y += 50.0f * deltaTime;
         if (state[SDL_SCANCODE_A]) rect.x -= 50.0f * deltaTime;
         if (state[SDL_SCANCODE_D]) rect.x += 50.0f * deltaTime;
+
+        spawnTimer += deltaTime;
+        if (spawnTimer >= spawnInterval) {
+            spawnTimer = 0.0f;
+        }
+
+        rect.y = screenHeight / 2.0f - rect.h / 2.0f;
 
         SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
         SDL_RenderFillRect(renderer, &rect);
