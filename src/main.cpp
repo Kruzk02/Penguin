c4394edf547a7c5dc3a29d15c6f268d69b4f47c8
Thien phuc <thienphuc484@gmail.com>
2026-03-15 14:05:45 +0700

    Add jumping

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 
 #include <SDL3/SDL_events.h>
+#include <SDL3/SDL_log.h>
 #include <SDL3/SDL_rect.h>
 #include <SDL3/SDL_render.h>
+#include <SDL3/SDL_scancode.h>
 
 #include "Game.h"
 
@@ -9,27 +11,48 @@ class Game1 : public Game {
     SDL_FRect rect{};
 
     const float spawnInterval = 2.0f;
-    float spawnTimer = 0.0f;
+    const float gravity = 250.0f;
 
-    void start() override { rect = {0, 0, (screenWidth - 400.0f) / 2, (screenHeight - 400.0f) / 2}; }
+    float centerY;
+    float spawnTimer = 0.0f;
+    float velocityY = 0.0f;
+
+    bool prevKeys[SDL_SCANCODE_COUNT] = {0};
+    bool isJumping = false;
+
+    void start() override {
+        rect = {0, 0, (screenWidth - 400.0f) / 2, (screenHeight - 400.0f) / 2};
+        centerY = screenHeight / 2.0f - rect.h / 2.0f;
+        rect.y = centerY;
+    }
 
     void update() override {
         SDL_PumpEvents();
 
         const bool* state = SDL_GetKeyboardState(nullptr);
 
-        if (state[SDL_SCANCODE_W]) rect.y -= 50.0f * deltaTime;
-        if (state[SDL_SCANCODE_S]) rect.y += 50.0f * deltaTime;
-        if (state[SDL_SCANCODE_A]) rect.x -= 50.0f * deltaTime;
-        if (state[SDL_SCANCODE_D]) rect.x += 50.0f * deltaTime;
+        if (state[SDL_SCANCODE_SPACE] && !prevKeys[SDL_SCANCODE_SPACE] && !isJumping) {
+            velocityY -= 450.0f;
+            isJumping = true;
+        }
+
+        velocityY += gravity * deltaTime;
+        rect.y += velocityY * deltaTime;
+
+        if (rect.y >= centerY) {
+            rect.y = centerY;
+            velocityY = 0;
+            isJumping = false;
+        } else if (rect.y <= centerY) {
+            rect.y += gravity * deltaTime;
+        }
 
         spawnTimer += deltaTime;
         if (spawnTimer >= spawnInterval) {
             spawnTimer = 0.0f;
         }
 
-        rect.y = screenHeight / 2.0f - rect.h / 2.0f;
-
+        prevKeys[SDL_SCANCODE_SPACE] = state[SDL_SCANCODE_SPACE];
         SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
         SDL_RenderFillRect(renderer, &rect);
     }
